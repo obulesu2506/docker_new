f827ad36d3040ef5ebb7edf2404a3e430e4cd4a1
kumar <obulesu.1406@gmail.com>
2025-02-25 18:35:27 -0500

    docker files updated

---
+++ dockerinfra/ec2.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "docker" {
+  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  vpc_security_group_ids = aws_security_group.allow_tls.id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "Docker"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name = "allow_tls_1"
+  description = "This Security group allow Inbound & Outbound SSH traffic"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls_1"
+  }
+}
