6bcfbb83b432c929da081fc230ef1f1451a2cd60
kumar <obulesu.1406@gmail.com>
2025-02-25 19:10:47 -0500

    docker files FROM, RUN, CMD

--- dockerinfra/ec2.tf
+++ dockerinfra/ec2.tf
@@ -1,34 +1,34 @@
 resource "aws_instance" "docker" {
   ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
-  vpc_security_group_ids = aws_security_group.allow_tls.id
-  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "t3.micro"
   tags = {
     Name = "Docker"
   }
 }
 
 resource "aws_security_group" "allow_tls" {
-  name = "allow_tls_1"
+  name        = "allow_tls_1"
   description = "This Security group allow Inbound & Outbound SSH traffic"
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
--- dockerinfra/provider.tf
+++ dockerinfra/provider.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "5.84.0"
+      source  = "hashicorp/aws"
+      version = "5.84.0"
     }
   }
 }
 
 provider "aws" {
-    #configuration options
-    region = "us-east-1"
-  
+  #configuration options
+  region = "us-east-1"
+
 }
