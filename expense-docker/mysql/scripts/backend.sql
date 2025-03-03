6ade65caebbd8d3995f1fd422d58481d0331302b
kumar <obulesu.1406@gmail.com>
2025-03-03 13:57:49 -0500

    updated expense project of mysql & backend components

--- expense-docker/mysql/scripts/backend.sql
+++ expense-docker/mysql/scripts/backend.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE IF NOT EXISTS transactions;
 USE transactions;
+
 CREATE TABLE IF NOT EXISTS transactions (
-    id INT AUTO_INCREMENT PRIMARY_KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     amount INT,
     description VARCHAR(255)
 );
-CREATE USER IF NOT EXISTS 'expense'@'%' identified by 'ExpenseApp@1';
+
+CREATE USER IF NOT EXISTS 'expense'@'%' IDENTIFIED BY 'ExpenseApp@1';
 GRANT ALL ON transactions.* TO 'expense'@'%';
 FLUSH PRIVILEGES;
